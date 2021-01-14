77d0f8b18fa15c28f69c8c12422a1f5eb1001805
Marta <marta.gilabert.guzman@gmail.com>
2021-01-14 18:48:21 +0100

    Iteration 1 to 3

--- src/movies.js
+++ src/movies.js
@@ -1,16 +1,63 @@
 // Iteration 1: All directors? - Get the array of all directors.
-// _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors. How could you "clean" a bit this array and make it unified (without duplicates)?
+
+function getAllDirectors(moviesArr){
+    let directorsArr = moviesArr.map((movie => {
+        return movie.director
+    }))
+    //console.log(moviesArr.length)
+    return directorsArr
+}
 
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
 
+const howManyMovies = (movies) => {
+    let spielbergDramas = movies.filter(el => {
+        if (el.director == "Steven Spielberg" && el.genre.includes('Drama')) {
+            return true;
+        }
+    })
+return spielbergDramas.length;
+}
+
 // Iteration 3: All rates average - Get the average of all rates with 2 decimals
+
+function ratesAverage(movies){
+    if (movies.length === 0){
+        return 0;
+    } 
+    let totalMovies = movies.reduce((acc, elem) => {
+        if (elem.rate === undefined){
+            return acc;
+        }
+        else {
+            return acc+elem.rate;
+        }
+    }, 0);
+    return +(totalMovies/movies.length).toFixed(2);
+}
+
 
 // Iteration 4: Drama movies - Get the average of Drama Movies
 
+
+
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
 
+function orderByYear(movies){
+    let clonedArray = JSON.parse(JSON.stringify(movies));
+}
 // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
 
-// BONUS - Iteration 7: Time Format - Turn duration of the movies from hours to minutes
-
-// BONUS - Iteration 8: Best yearly rate average - Best yearly rate average
+function orderAlphabetically(movies){
+    let clonedArray = JSON.parse(JSON.stringify(movies));
+    clonedArray.sort(function(a,b){
+        if (a.title > b.title){
+            return 1;
+        } else if (a.title < b.title){
+            return -1;
+        } else {
+            return 0;
+        }
+    })
+    return clonedArray.slice(0,20)
+};
